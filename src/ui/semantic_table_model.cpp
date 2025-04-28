c71d73dd86db296a9b56302d7b7ce5d8a71432c6
Mateus Silva <malockeybr@gmail.com>
2025-04-27 21:10:16 -0300

    feat: add new datas examples from table

--- src/ui/semantic_table_model.cpp
+++ src/ui/semantic_table_model.cpp
@@ -19,12 +19,19 @@ SemanticTableModel::SemanticTableModel(QObject *parent)
 }
 
 void SemanticTableModel::populateModel() {
-  semanticTable.push_back({"token1", "INT", 10}); // exemplos pra tabela
-  semanticTable.push_back({"token2", "FLOAT", 20});
-
-  setHorizontalHeaderLabels({ "Token", "Tipo", "Valor" }); // colunas
-
-  for (const auto& entry : semanticTable) {
+    // dados de exemplo por enquanto
+    std::vector<QStringList> data = {
+        {"token1", "INT", "10", "1", "1"},
+        {"token2", "FLOAT", "20", "2", "5"},
+        {"token3", "DOUBLE", "30.5", "3", "10"},
+        {"token4", "NUMBER", "40", "4", "15"},
+        {"token5", "FLOAT", "50.75", "5", "20"},
+        {"token6", "INT", "60", "6", "25"},
+        {"token7", "DOUBLE", "70.25", "7", "30"},
+        {"token8", "NUMBER", "80", "8", "35"},
+        {"token9", "FLOAT", "90.5", "9", "40"},
+        {"token10", "INT", "100", "10", "45"}
+    };
       QList<QStandardItem*> rowItems;
       rowItems.append(new QStandardItem(QString::fromStdString(entry.token)));
       rowItems.append(new QStandardItem(QString::fromStdString(entry.tipo)));
