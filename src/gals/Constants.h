e5ccffd54d11da4c68624e6122c055ea7df54ece
mateusbarbosa <barbosamat16@gmail.com>
2025-05-04 13:21:05 -0300

    Feat: add action #3 for management of declarations

--- src/gals/Constants.h
+++ src/gals/Constants.h
@@ -100,10 +100,10 @@ const int ACCEPT = 3;
 const int GO_TO  = 4;
 const int ERROR  = 5;
 
-extern const int PARSER_TABLE[344][191][2];
+extern const int PARSER_TABLE[345][191][2];
 
 extern const int PRODUCTIONS[156][2];
 
-extern const char *PARSER_ERROR[344];
+extern const char *PARSER_ERROR[345];
 
 #endif
