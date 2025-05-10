70a2b6dc5519882f0c9b7db948803fcafb1f6c7a
Jonathas Meine <jonhy.meine@gmail.com>
2025-05-10 12:10:36 -0300

    style: apply 4 spaces indentation in SemanticError

--- src/gals/SemanticError.h
+++ src/gals/SemanticError.h
@@ -11,92 +11,93 @@ using namespace std;
 class SemanticError : public AnalysisError
 {
 public:
-  SemanticError(const string &msg, int position = -1)
-      : AnalysisError(msg, position) {}
+    SemanticError(const string &msg, int position = -1)
+        : AnalysisError(msg, position) {}
 
-  // Static helpers for common errors
-  static string TypeMismatch(SemanticTable::Types expected, SemanticTable::Types actual)
-  {
-    return "Type mismatch: expected '" + typeToString(expected) + "' but got '" + typeToString(actual) + "'";
-  }
+    // Static helpers for common errors
+    static string TypeMismatch(SemanticTable::Types expected, SemanticTable::Types actual)
+    {
+        return "Type mismatch: expected '" + typeToString(expected) + "' but got '" + typeToString(actual) + "'";
+    }
 
-  static string SymbolNotOfClassification(const string &id)
-  {
-    return "Symbol '" + id + "' is not of the expected classification";
-  }
+    static string SymbolNotOfClassification(const string &id)
+    {
+        return "Symbol '" + id + "' is not of the expected classification";
+    }
 
-  static string DuplicateSymbol(const string &id)
-  {
-    return "Symbol '" + id + "' already exists in this scope";
-  }
+    static string DuplicateSymbol(const string &id)
+    {
+        return "Symbol '" + id + "' already exists in this scope";
+    }
 
-  static string TypeAssignmentMismatch(const string &id, SemanticTable::Types expected)
-  {
-    return "Type mismatch: cannot assign to variable '" + id + "' of type '" + to_string(expected) + "'";
-  }
+    static string TypeAssignmentMismatch(const string &id, SemanticTable::Types expected)
+    {
+        return "Type mismatch: cannot assign to variable '" + id + "' of type '" + to_string(expected) + "'";
+    }
 
-  static string ReturnOutsideFunction()
-  {
-    return "Return statement outside of a function";
-  }
+    static string ReturnOutsideFunction()
+    {
+        return "Return statement outside of a function";
+    }
 
-  static string SymbolUndeclared(const string &id)
-  {
-    return "Symbol '" + id + "' is not declared";
-  }
-  
-  static string SymbolNotInitialized(const string &id)
-  {
-    return "Symbol '" + id + "' was not initialized";
-  }
+    static string SymbolUndeclared(const string &id)
+    {
+        return "Symbol '" + id + "' is not declared";
+    }
 
-  static string InputNonVariable(const string &id)
-  {
-    return "Cannot input into non-variable '" + id + "'";
-  }
+    static string SymbolNotInitialized(const string &id)
+    {
+        return "Symbol '" + id + "' was not initialized";
+    }
 
-  static string ExpressionStackEmpty()
-  {
-    return "Expression stack is empty";
-  }
+    static string InputNonVariable(const string &id)
+    {
+        return "Cannot input into non-variable '" + id + "'";
+    }
 
-  static string NonNumericOperator()
-  {
-    return "Arithmetic operator requires numeric operands";
-  }
+    static string ExpressionStackEmpty()
+    {
+        return "Expression stack is empty";
+    }
 
-  static string oneOperatorForBinaryExpression()
-  {
-    return "Binary expression requires two operands";
-  }
+    static string NonNumericOperator()
+    {
+        return "Arithmetic operator requires numeric operands";
+    }
 
-  static string WrongArgumentCount(const string &id, int expected, int actual)
-  {
-    return "Function '" + id + "' expects " + to_string(expected) + " argument(s), but got " + to_string(actual);
-  }
+    static string oneOperatorForBinaryExpression()
+    {
+        return "Binary expression requires two operands";
+    }
 
-  static string InvalidValueForArrayLength()
-  {
-    return "Invalid value for array length";
-  }
+    static string WrongArgumentCount(const string &id, int expected, int actual)
+    {
+        return "Function '" + id + "' expects " + to_string(expected) + " argument(s), but got " + to_string(actual);
+    }
 
-  static string typeToString(SemanticTable::Types type)
-  {
-    if (type == SemanticTable::Types::__NULL)
-      return "__NULL";
-    else if (type == SemanticTable::Types::INT)
-      return "INT";
-    else if (type == SemanticTable::Types::FLOAT)
-      return "FLOAT";
-    else if (type == SemanticTable::Types::DOUBLE)
-      return "DOUBLE";
-    else if (type == SemanticTable::Types::CHAR)
-      return "CHAR";
-    else if (type == SemanticTable::Types::STRING)
-      return "STRING";
-    else if (type == SemanticTable::Types::BOOL)
-      return "BOOL";
-  }
+    static string InvalidValueForArrayLength()
+    {
+        return "Invalid value for array length";
+    }
+
+
+    static string typeToString(SemanticTable::Types type)
+    {
+        if (type == SemanticTable::Types::__NULL)
+            return "__NULL";
+        else if (type == SemanticTable::Types::INT)
+            return "INT";
+        else if (type == SemanticTable::Types::FLOAT)
+            return "FLOAT";
+        else if (type == SemanticTable::Types::DOUBLE)
+            return "DOUBLE";
+        else if (type == SemanticTable::Types::CHAR)
+            return "CHAR";
+        else if (type == SemanticTable::Types::STRING)
+            return "STRING";
+        else if (type == SemanticTable::Types::BOOL)
+            return "BOOL";
+    }
 };
 
 #endif
