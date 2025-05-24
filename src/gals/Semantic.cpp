f8f24890a8ecb058c886ebb62710693911fff026
mateusbarbosa <barbosamat16@gmail.com>
2025-05-24 14:04:47 -0300

    fix: remove redundant initialization check for declared variable

--- src/gals/Semantic.cpp
+++ src/gals/Semantic.cpp
@@ -185,11 +185,6 @@ void Semantic::executeAction(int action, const Token *token)
             symbolTable.getSymbol(this->currentSymbol->id);
 
         validateIfVariableIsDeclared(matchedSymbol, this->currentSymbol->id);
-
-        // TODO: cannot allow the syntax bellow
-        // int a
-        // a = a;
-        matchedSymbol->isInitialized = true;
         this->declarationSymbol = matchedSymbol;
 
         break;
